49e70e0aaaeaee5115be7a484f35f53e36e49752
Steph Nugs <steph.bowie1993@gmail.com>
2023-05-25 22:53:41 -0600

    created function that checks if element is an array

---
+++ flattern.js
@@ -0,0 +1,44 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+  }
+}; 
+
+const flatten = function(array) {
+  const flattenedArray = [];
+
+  for (let element of array) {
+    if (Array.isArray(element)) {
+      flattenedArray.push(...element);
+    } else {
+      flattenedArray.push(element);
+    }
+  }
+
+  return flattenedArray;
+};
+console.log(flatten([1, 2, [3, 4], 5, [6]]));
