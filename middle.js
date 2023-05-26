ab8eebed8290ec0b9eb4c915a2b085f85dc8a3c0
Steph Nugs <steph.bowie1993@gmail.com>
2023-05-25 22:59:59 -0600

    created function that returns the middle elemnets of an array

---
+++ middle.js
@@ -0,0 +1,50 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+  }
+}; 
+
+const middle = function(array) {
+  const length = array.length;
+  if (length <= 2) {
+    return [];
+  } else if (length % 2 === 1) {
+    const middleIndex = Math.floor(length / 2);
+    return [array[middleIndex]];
+  } else {
+    const middleIndex1 = length / 2 - 1;
+    const middleIndex2 = length / 2;
+    return [array[middleIndex1], array[middleIndex2]];
+  }
+};
+
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
