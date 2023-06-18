344b9f0ee3c1d866058b17d58f9832d8d52efd45
Steph Nugs <steph.bowie1993@gmail.com>
2023-06-17 18:35:05 -0600

    added comments to better understand code, and tested a new console log

--- flattern.js
+++ flattern.js
@@ -28,17 +28,18 @@ const assertArraysEqual = function(array1, array2) {
   }
 }; 
 
-const flatten = function(array) {
+const flatten = function(array) { // defines function flatten that takes in an array as a parameter and returns a new array 
   const flattenedArray = [];
 
-  for (let element of array) {
-    if (Array.isArray(element)) {
-      flattenedArray.push(...element);
+  for (let element of array) { // for of loop that checks if each element is an array 
+    if (Array.isArray(element)) { // checks if 'element' array is indeed an array
+      flattenedArray.push(...element); // '...' is a spread opperator, it expands the nested array into its individual elements. Then pushes the nested array 'element' into the flattenedArray
     } else {
-      flattenedArray.push(element);
+      flattenedArray.push(element); // if element is not an array, it pushes the element to the flattenedArray
     }
   }
 
   return flattenedArray;
 };
 console.log(flatten([1, 2, [3, 4], 5, [6]]));
+console.log(flatten([1, 5, ['I', 'love', 'Miguel O\' Hara'], 9, [7]]))
