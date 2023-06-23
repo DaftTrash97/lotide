4665ae2b570b2d6b567c4c13d4a465e5af81f6ad
Steph Nugs <steph.bowie1993@gmail.com>
2023-06-23 01:53:41 -0600

    missunderstood instructions, re-wrote function

--- letterPositions.js
+++ letterPositions.js
@@ -1,11 +1,11 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// };
 
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
@@ -29,19 +29,21 @@ const assertArraysEqual = function(array1, array2) {
 }; 
 
 const letterPositions = function(sentence) {
-  const results = {};
-  for (let i = 0; i < sentence.length; i++) {// creates a loop that goes through each letter starting at index 0
-    const character = sentence[i]; // declares new const 'character' and assigns it the value of the character at current index 'i' in the sentence string
-    if (character !== ' ') { // if statement that checks if the character is not equal to a space
-      if (results[character]) { // if statment that exicutes when the character is already a property in the 'results' object and is not a space
-      results[character].push(i); // adds the index 'i' to the array assositated with 'character' in the 'results' object
-      } else { // else statment that exicutes when a character is not a space and dosen't already esist in the reults object 
-        results[character] = [i]; // creates a new property in the results object and assigns and array with the current index 'i'
-      }
+  let results = {};
+  //loop through sentence 
+  for (let i = 0; i < sentence.length; i++) {
+    //checks if results at [i] is true 
+    if (results[sentence[i]]) {
+      //if it is true then the sentence[letterPos] 's array of positions gets pushed the new [letterPos]
+      results[sentence[i]].push(i)
+    }
+    else {
+      //else if create new property with letter as its key and index as its value
+      results[sentence[i]] = [i]
     }
   }
   return results;
 };
 console.log(letterPositions("lighthouse in the house"));
 
-assertArraysEqual(letterPositions("hello").e, [1]);
+ assertArraysEqual(letterPositions("hello").e, [1]);
