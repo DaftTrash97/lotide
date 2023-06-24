3e07ddfc6e8e483af77108cf3487dda12bc3e721
Steph Nugs <steph.bowie1993@gmail.com>
2023-06-24 16:21:39 -0600

    fixed for in loop to insure code continues to loop through objects even if the first key is true

--- eqObjects.js
+++ eqObjects.js
@@ -21,20 +21,20 @@ const eqArrays = function(array1, array2) {
 };
 
 const eqObjects = function(obj1, obj2) {
-  const objectLengths = Object.keys(obj1).length === Object.keys(obj2).length
-  
-  for (key in obj1) {
+  const objectLengths = Object.keys(obj1).length === Object.keys(obj2).length;
+
+  for (let key in obj1) {
     if (Array.isArray(obj1[key]) && Array.isArray(obj2[key])) {
-      eqArrays(obj1[key], obj2[key])
-    }
-    else if (obj1[key] === obj2[key] && objectLengths) {
-      return true
-    }
-    else {
-      return false
+      if (!eqArrays(obj1[key], obj2[key])) {
+        return false;
+      }
+    } else if (obj1[key] !== obj2[key]) {
+      return false;
     }
   }
- }
+
+  return true;
+};
 
 
 const shirtObject = { color: "red", size: "medium" };
