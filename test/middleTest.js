47965d40236d1e150bc9133ff351a17b4cfea735
Steph Nugs <steph.bowie1993@gmail.com>
2023-07-05 21:43:30 -0600

    refactored code and created a test file

---
+++ test/middleTest.js
@@ -0,0 +1,13 @@
+const assertEqual = require('../assertEqual')
+const eqArrays = require('../eqArrays')
+const assertArraysEqual = require('../assertArraysEqual')
+const middle = require('../middle')
+
+//TEST CODE 
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+assertArraysEqual(middle([7, 7, 6, 7, 8]), [6]);
