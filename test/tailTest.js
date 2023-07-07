e880afa6c5220829571fdd1b49cdbf486ff61e41
Steph Nugs <steph.bowie1993@gmail.com>
2023-07-06 18:35:37 -0600

    refactored code to use moach and chai for testig

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,9 +1,21 @@
+const assert = require('chai').assert;
 const assertEqual = require('../assertEqual')
 const tail = require('../tail')
+const result = tail(["Hello", "Lighthouse", "Labs", "I", "am", "tired"]);
 
 //TEST CODE
-const result = tail(["Hello", "Lighthouse", "Labs", "I", "am", "tired"]);
-console.log(result)
-assertEqual(result.length, 5);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[4], "tired");
+
+describe("#tail", () => {
+  it("returns 'tired' for [4]", () => {
+    assert.deepEqual(result[4], 'tired');
+  });
+
+  it("returns 'Lighthouse' for [0]", () => {
+    assert.deepEqual(result[0], 'Lighthouse');
+  });
+
+  it("returns 5 for result.length", () => {
+    assert.deepEqual(result.length, 5);
+  });
+
+});
