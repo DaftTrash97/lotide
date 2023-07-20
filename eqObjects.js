8ac2ff871a824092879f29e863eec63119702b48
Steph Nugs <steph.bowie1993@gmail.com>
2023-07-20 13:29:18 -0600

    fixed bug in JSON package

--- eqObjects.js
+++ eqObjects.js
@@ -3,13 +3,12 @@ const eqArrays = require('./eqArrays');
 
 const eqObjects = function(obj1, obj2) {
 
-const obj1Keys = Object.keys(obj1);
-const obj2Keys = Object.keys(obj2);
+  const obj1Keys = Object.keys(obj1);
+  const obj2Keys = Object.keys(obj2);
 
-if (obj1Keys.length !== obj2Keys.length) {
-  return false;
-}
-
+  if (obj1Keys.length !== obj2Keys.length) {
+    return false;
+  }
 
   for (let key in obj1) {
     if (Array.isArray(obj1[key]) && Array.isArray(obj2[key])) {
